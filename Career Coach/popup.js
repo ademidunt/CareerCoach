3a431c3dcd324ae04020c007352d67665f332043
Tanya Nantsa <tanyanantsa@gmail.com>
2022-01-09 03:29:09 -0500

    one example job

--- Career Coach/popup.js
+++ Career Coach/popup.js
@@ -1,20 +1,57 @@
 document.addEventListener('DOMContentLoaded', function (){
     document.querySelector('button').addEventListener('click', saveJob, false)
+    
+    // Storage.prototype.setObj = function(key, obj) {
+    //     return this.setItem(key, JSON.stringify(obj))
+    // }
+    // Storage.prototype.getObj = function(key) {
+    //     return JSON.parse(this.getItem(key))
+    // }
+    
+ 
+    //alert(tab);
 
-    Storage.prototype.setObj = function(key, obj) {
-        return this.setItem(key, JSON.stringify(obj))
-    }
-    Storage.prototype.getObj = function(key) {
-        return JSON.parse(this.getItem(key))
-    }
+    chrome.windows.getCurrent(function (currentWindow) {
+        chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
+            activeTabs.map(function (tab) {
+                chrome.tabs.executeScript(tab.id, { file: 'content_script.js', allFrames: false });
+            });
+        });
+    });
+
+    console.log(tabs[0].url);
+    
     function saveJob() {
         // save something to local storage
         //use array pop and push functions
+
+       // var link = tab;
+        //localStorage.setItem('Link', link);
+
         var title = document.getElementById("jobTitle").value;
-        var jobtitle = [];
-        jobtitle = localStorage.getObj('jobTitle');
-        jobtitle.push(title);
-        localStorage.setObj('jobTitle', jobtitle);
+        // var jobtitle = [];
+        // jobtitle = localStorage.getItem('jobTitle');
+        // jobtitle.push(title);
+        localStorage.setItem('JobTitle', title);
         //alert('Grr.')
+
+        var location = document.getElementById("jobLoc").value;
+        // var jobloc = [];
+        // jobloc = localStorage.getItem('jobLoc');
+        //jobloc.push(location);
+        localStorage.setItem('JobLoc', location);
+        
+        var location = document.getElementById("jobDescript").value;
+        localStorage.setItem('JobDescript', location);
+
+        var deadline = document.getElementById("deadline").value;
+        localStorage.setItem('Deadline', deadline);
+
+
+        var applied = document.getElementById("Applied").value;
+        localStorage.setItem('App', applied);
     }
+    
+    
+
 }, false)
--- Career Coach/saved.js
+++ Career Coach/saved.js
@@ -1,65 +1,61 @@
 document.addEventListener('DOMContentLoaded', function (){
     //alert('Grr.')
-
-    Storage.prototype.setObj = function(key, obj) {
-        return this.setItem(key, JSON.stringify(obj))
-    }
-    Storage.prototype.getObj = function(key) {
-        return JSON.parse(this.getItem(key))
-    }
+    //localStorage.clear();
+    // Storage.prototype.setObj = function(key, obj) {
+    //     return this.setItem(key, JSON.stringify(obj))
+    // }
+    // Storage.prototype.getObj = function(key) {
+    //     return JSON.parse(this.getItem(key))
+    // }
     // when page loads get what is local storage to populate page
-    const cat = localStorage.getObj('jobTitle');
+    const link = localStorage.getItem('Link');
+    const cat = localStorage.getItem('JobTitle');
+    const loc = localStorage.getItem('JobLoc');
+    const des = localStorage.getItem('JobDescript');
+    const deadline = localStorage.getItem('Deadline');
+    const app = localStorage.getItem('App');
+
     var table = document.getElementById("myTable");
     //or add to an array first son all the jobs get saved
         var index = 1;
         // var row;
-        // var cell1;
+        //var cell2;
 
-        // var row = table.insertRow(1);
-        // var cell1 = row.insertCell(0);
-        // var cell2 = row.insertCell(1);
-        // var cell3 = row.insertCell(2);
-        // var cell4 = row.insertCell(3);
-        // var cell5 = row.insertCell(4);
-        // var cell6 = row.insertCell(5);
-        // cell1.innerHTML = cat[2];
-        // cell2.innerHTML = "cat test";
-        // cell3.innerHTML = "cat test";
-        // cell4.innerHTML = "cat test";
-        // cell5.innerHTML = "cat test";
-        // cell6.innerHTML = "cat test";
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML = link;
+        cell2.innerHTML = cat;
+        cell3.innerHTML = loc;
+        cell4.innerHTML = des;
+        cell5.innerHTML = deadline;
+        cell6.innerHTML = app;
 
-        // var row = table.insertRow(2);
-        // var cell1 = row.insertCell(0);
-        // var cell2 = row.insertCell(1);
-        // var cell3 = row.insertCell(2);
-        // var cell4 = row.insertCell(3);
-        // var cell5 = row.insertCell(4);
-        // var cell6 = row.insertCell(5);
-        // cell1.innerHTML = cat[1];
-        // cell2.innerHTML = "cat test";
-        // cell3.innerHTML = "cat test";
-        // cell4.innerHTML = "cat test";
-        // cell5.innerHTML = "cat test";
-        // cell6.innerHTML = "cat test";
 
-        for (let i = 0; i < cat.length; i++) {
-            var row = table.insertRow(index);
-            //var cell1 = row.insertCell(i);
-            //var cell1.innerHTML = cat[i];
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(4);
-            var cell6 = row.insertCell(5);
-            cell1.innerHTML = cat[i];
-            cell2.innerHTML = "cat test";
-            cell3.innerHTML = "cat test";
-            cell4.innerHTML = "cat test";
-            cell5.innerHTML = "cat test";
-            cell6.innerHTML = "cat test";
-            index += 1;
-        }
-        Storage.clear();
+        // for (let i = 0; i < cat.length; i++) {
+        //     var row = table.insertRow(index);
+        //     //var cell1 = row.insertCell(i);
+        //     //var cell1.innerHTML = cat[i];
+        //     var cell1 = row.insertCell(0);
+        //     cell2 = row.insertCell(1);
+        //     var cell3 = row.insertCell(2);
+        //     var cell4 = row.insertCell(3);
+        //     var cell5 = row.insertCell(4);
+        //     var cell6 = row.insertCell(5);
+        //     cell1.innerHTML = cat[i];
+        //     cell2.innerHTML = "loc[i]";
+        //     cell3.innerHTML = "cat test";
+        //     cell4.innerHTML = "cat test";
+        //     cell5.innerHTML = "cat test";
+        //     cell6.innerHTML = "cat test";
+        //     index += 1;
+        // }
+        // for (let x = 0; x < loc.length; x++) {
+        //     cell2.innerHTML = loc[x];
+        // }
+        //Storage.clear();
 }, false)
